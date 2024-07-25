4d38769acd2dd34946dc93d9c9d736989883f3c9
Paul Mozeley <pdreier15@gmail.com>
2024-07-25 14:22:07 -0400

    initial commit

---
+++ words.py
@@ -0,0 +1,16 @@
+words = ['cat','hat','mat','flat','rat','tat']
+
+
+def must_start_with(a,b):
+    for word in words:
+        if word[0] == a or word[0] == b:
+            print(word)
+
+def print_upper_words(words,func):
+    """Will print 1 word per line until it reaches the the end of the list"""
+    must_start_with
+
+
+
+print_upper_words(words,must_start_with('h','r'))
+
